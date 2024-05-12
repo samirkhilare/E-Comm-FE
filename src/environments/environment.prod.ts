14c4f6990a1ab733dace92ab46af102255e7e554
spmshipping <samirkhilare3535@gmail.com>
2024-05-12 21:19:16 +0530

    intial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://16.171.11.7:8080/api/v1/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://16.171.11.7:8080/api/v1/',
+};
+
+
